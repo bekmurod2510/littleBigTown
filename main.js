e803289f5d7160b24d1017278fb8c108ac4f81d2
Bekmurod O'ktamov <bekmurod2510@gmail.com>
2026-09-21 22:20:01 +0500

    init

---
+++ main.js
@@ -0,0 +1,36 @@
+import * as THREE from 'three';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 500);
+camera.position.set(0, 0, 100);
+camera.lookAt(0, 0, 0);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+/* 
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+*/
+
+const material = new THREE.LineBasicMaterial({ color: 0x0000ff });
+const points = []
+points.push(new THREE.Vector3(-10, 0, 0))
+points.push(new THREE.Vector3(0, 10, 0))
+points.push(new THREE.Vector3(10, 0, 0))
+points.push(new THREE.Vector3(0, -10, 0))
+points.push(new THREE.Vector3(-10, 0, 0))
+
+const geometry = new THREE.BufferGeometry().setFromPoints(points);
+const line = new THREE.Line(geometry, material);
+scene.add(line);
+
+
+function animate(time) {
+  renderer.render(scene, camera);
+}
+
+renderer.setAnimationLoop(animate);
